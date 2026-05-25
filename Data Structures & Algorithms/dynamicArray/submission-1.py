0d87ff374ec94e8267e7b577929ce6c3e2631c43
RoryGlenn <32777747+RoryGlenn@users.noreply.github.com>
2026-05-25 15:46:59 -0400

    Bulk sync: 2 submissions

---
+++ Data Structures & Algorithms/dynamicArray/submission-1.py
@@ -0,0 +1,33 @@
+class DynamicArray:
+    def __init__(self, capacity: int):
+        self.array = [0] * capacity
+        self.capacity = capacity
+        self.size = 0
+
+    def get(self, i: int) -> int:
+        return self.array[i]
+
+    def set(self, i: int, n: int) -> None:
+        self.array[i] = n
+
+    def pushback(self, n: int) -> None:
+        if self.size == self.capacity:
+            self.resize()
+        self.array[self.size] = n
+        self.size += 1
+
+    def popback(self) -> int:
+        result = self.array[self.size-1]
+        self.array[self.size-1] = 0
+        self.size -= 1
+        return result
+
+    def resize(self) -> None:
+        self.array = self.array + [0] * self.capacity
+        self.capacity *= 2
+
+    def getSize(self) -> int:
+        return self.size
+    
+    def getCapacity(self) -> int:
+        return self.capacity
